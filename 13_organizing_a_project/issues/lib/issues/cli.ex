082f090250f2da66f55768d8f8550d774ec70ca9
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-08-15 08:52:00 -0300

    add chapter 13

---
+++ 13_organizing_a_project/issues/lib/issues/cli.ex
@@ -0,0 +1,56 @@
+defmodule Issues.CLI do
+
+    import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
+    @default_count 4
+
+    def run(argv) do
+        argv
+            |> parse_args
+            |> process
+    end
+
+    def parse_args(argv) do
+        parse = OptionParser.parse(argv, switches: [help: :boolean],
+                                        aliases: [h: :help ])
+
+        case parse do
+            {[ help: true ], _, _} -> :help
+            {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
+            {_, [user, project], _} -> {user, project, 4}
+            _ -> :help
+        end
+    end
+
+    def process(:help) do
+        IO.puts """
+        usage: issues <user> <project> [count | #{@default_count}]
+        """
+    end
+
+    def process({user, project, count}) do
+        Issues.GithubIssues.fetch(user, project)
+            |> decode_response
+            |> convert_to_list_of_hashdicts
+            |> sort_into_ascending_order
+            |> Enum.take(count)
+            |> print_table_for_columns(["number", "created_at", "title"])
+    end
+
+    def decode_response({:ok, body}), do: body
+    def decode_response({:error, error}) do
+        {_, message} = List.keyfind(error, "message", 0)
+        IO.puts "Error fetching from GitHub: #{message}"
+        System.halt(2)
+    end 
+
+    def convert_to_list_of_hashdicts(list) do
+        list
+            |> Enum.map(&Enum.into(&1, HashDict.new))
+            # to give fast access by key
+    end
+
+    def sort_into_ascending_order(list_of_issues) do
+        Enum.sort list_of_issues, &(&1["created_at"] <= &2["created_at"])
+    end
+end
